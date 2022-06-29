7fcad7ac8ff06d0166119910202b894562127a69
takabo <thousandbeach.td@gmail.com>
2022-06-29 22:30:50 +0900

    create the header section

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import User from './User';
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className='flex justify-between p-5 text-sm text-gray-700'>
+          <div className='flex items-center space-x-4'>
+            <p className='link'>About</p>
+            <p className='link'>Store</p>
+          </div>
+          <div className='flex items-center space-x-4'>
+            <p className='link'>Gmail</p>
+            <p className='link'>Images</p>
+            <User />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
